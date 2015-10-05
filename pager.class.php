bd4ea81ccd8255e8d2c2fb52f2167bb589a7d96d
Alison Monteiro <alisonmonteiro@MacBook-Pro-de-Alison-monteiro.local>
2015-10-04 22:49:31 -0300

    Update the Pager class structure

--- pager.class.php
+++ pager.class.php
@@ -7,91 +7,115 @@
  * @link       https://github.com/BrunnoFraga/pager
  * @since      2015
  *
- * @param Array $list
- * @param Int $p
- * @param Int $view
- * @param String $href
- * @param String $url 
- * @param String $a 
- *
-*/
+ */
 
-
-class pager
+class Pager
 {
-	private $list,$p,$view,$total,$n; //VARIABLES
-
-	function __construct($list,$p,$view) // CONSTRUCT FUNCTION, USED IN CREATE METHOD: $pager = new pager($list,$p,$view);
-	{
-		
-		foreach($list as $v) if(is_array($v) || is_object($v)) $one = true; else $one = false; // CHECK $list SIZE ARRAY
-
-		$this->p = $p; // $p IS THE POINTER OF ACTIVE PAGER
-		$this->view = $view; // NUMBER OF ITENS TO SHOW
-		
-		if($one===false){ // JUST ONE RESULTS
-
-			$this->total = 1;
-			$this->list[0] = $list; 
-
-		}else{ // MULTIPLE RESULTS
-
-			$this->total = count($list);	
-
-			for($i=($p-1)*$view;$i<($p*$view);$i++)
-			{
-				if(isset($list[$i]))
-				{
-					$this->list[$i] = $list[$i];				
-				}
-			}
-
-		}
-
-		
-	}
-
-	function get($a)
-	{
-		return $this->$a;
-	}
+    private $list;
+    private $p;
+    private $view;
+    private $total;
 
 
-	function pager($href="",$url="") // DEFAULT - YOU DONT NEED ANY PARAM BUT IF YOU NEED A PERMALINK TRY: pager("href","yourpage.html") 
-	{
-		$li = "";
-		$i = 1;
-		$a = ""; 
-		$aa="";
-		
+    /**
+     * Pager object creation
+     *
+     * @param Array $list
+     * @param Int $p : The page number
+     * @param Int $view : Itens to show
+     * @return Pager Object
+     */
+    public function __construct(Array $list, $p, $view)
+    {
+        $this->p = (int) $p;
+        $this->view = $view;
+        $this->total = count($list);
 
-		if($this->total > $this->view)
-		{
-			
-			while($i <= ceil($this->total/$this->view))
-			{
-				if(($i < ($this->p+7) && $i > ($this->p-7)) || $i == ceil($this->total / $this->view) || $i == 1) // 7 IS THE NUMBER TO ABBREVIATE THE ITENS OF PAGER 1 ... 2 3 4 5 6 ... 7 USED FOR HIGH AMOUNTS LIST
-				{
-					// HREF
-					if($href == "href"){$a='<a class="'.(($i==$this->p)?"active":"").'" href="/'.$url.'?p='.$i.'" target="_self" >';$aa="</a>";}
-					
-					//...
-					$last = ($i == ceil($this->total/$this->view))?"...":"";
-					$first = ($i == 1)?"...":"";
+        if ($this->total > 1) {
+            for ($i = ($p - 1) * $view; $i < ($p * $view); $i++) {
+                if (isset($list[$i])) {
+                    $this->list[$i] = $list[$i];
+                }
+            }
+        } else {
+            $this->list[0] = $list;
+        }
 
-					// LI
-					$li .= $last.$a.'<li class="'.(($i==$this->p)?"active":"").'">'.$i.'</li>'.$aa.$first;
-				}
-
-				$i++;
-			}
-		
-		}
-
-		$pager = '<ul class="pager">'.$li.'</ul>'; // THE RESULT <ul class="pager"><li class="active">1</li><li>2</li><li>3</li></ul>
-		return $pager;
-	}
+    }
 
 
-	
+    /**
+     * Get an class' property
+     *
+     * @param $property
+     * @return mixed
+     */
+    public function get($property)
+    {
+        return isset($this->$property) ? $this->$property : false;
+    }
+
+
+    /**
+     * This method show your pagination
+     * The result must be something like
+     * <ul class="pager"><li class="active">1</li><li>2</li><li>3</li></ul>
+     *
+     * @param String $url: The prefix url
+     * @return string
+     */
+    function show($url = '')
+    {
+        $li = '';
+
+        if ($url === '') {
+            $url = $_SERVER['PHP_SELF'];
+        }
+
+        if ($this->total > $this->view) {
+            $i = 1;
+
+            while ($i <= ceil($this->total / $this->view)) {
+
+                // 7 IS THE NUMBER TO ABBREVIATE THE ITENS OF PAGER 1 ... 2 3 4 5 6 ... 7 USED FOR HIGH AMOUNTS LIST
+                if (($i < ($this->p + 7) && $i > ($this->p - 7)) || $i == ceil($this->total / $this->view) || $i == 1) {
+
+                    $first = ($i == 1) ? '...' : '';
+                    $last = ($i == ceil($this->total / $this->view)) ? '...' : '';
+
+                    $active = !$this->activePage($i) ? '' : 'active';
+
+                    $link = '<a class="page-number-' . $i . '" href="' . $url . '?p=' . $i . '">';
+                    $link .= $i;
+                    $link .= '</a>';
+
+                    // The <li> element
+                    $li .= $last;
+                    $li .= '<li class="' . $active . '">' . $link . '</li>';
+                    $li .= $first;
+                }
+
+                $i++;
+            }
+
+        }
+
+
+        return printf('<ul class="pager">%s</ul>', $li);
+    }
+
+
+    /**
+     * Helps you to know if you are in the current page
+     * Maybe this method must be used in loops.
+     *
+     * @param int $counter
+     * @return bool
+     */
+    private function activePage ($counter) {
+        if ((int) $counter === $this->p) return true;
+
+        return false;
+    }
+
 }
